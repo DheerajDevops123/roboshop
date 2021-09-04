fb49f0b1b7cd6d185937ae0ea66e49cf9cc8add3
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-04 16:56:15 +0000

    roboshop

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,4 +1,4 @@
-echo "Setting up MongoDB"
+echo "Setting up MongoDB Repository"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -8,7 +8,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 
 echo "Installing MongoDB"
-yum install -y mongodb-org >/tmp/log
+yum install -y mongodb-org >>/tmp/log
 
 echo "Configuring MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
@@ -22,8 +22,8 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
 
 cd /tmp
 echo "Extracting Schema"
-unzip -o mongodb.zip >/tmp/log
+unzip -o mongodb.zip >>/tmp/log
 cd mongodb-main
 echo "Loading Schema"
-mongo < catalogue.js >/tmp/log
-mongo < users.js >/tmp/log
+mongo < catalogue.js >>/tmp/log
+mongo < users.js >>/tmp/log
