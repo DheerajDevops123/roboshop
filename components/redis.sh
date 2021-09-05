fa4c42b8027fd6530bf85c04eb959472b92eb60f
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-05 16:10:26 +0000

    mongopush2

--- components/redis.sh
+++ components/redis.sh
@@ -14,7 +14,7 @@ yum install redis -y &>>$LOG
 STATUS $?
 
 print "Configuring Redis"
-sed -i -e "s/127.0.0.1/0.0.0.0" /etc/redis.conf
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf
 STATUS $?
 
 print "Start Redis Database"
