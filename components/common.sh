e029d9c0440245583af4be61b92153b739e05112
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-05 09:50:00 +0000

    mongopush2

---
+++ components/common.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+STATUS() {
+if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+fi
+}
+
+print() {
+    echo -n -e "$1 \t- "
+}
+
+if [ $UID -ne 0 ]; then
+echo -e "/e[1;33mYou Should execute this script as the root User"
+exit 1
+fi
