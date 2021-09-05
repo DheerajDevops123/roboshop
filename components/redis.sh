6c3b3ba3f1ba54fe4aaa63dbb87e03f2a5c511ad
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-05 16:10:59 +0000

    mongopush2

--- components/redis.sh
+++ components/redis.sh
@@ -5,18 +5,18 @@ print "Install Yum Utils & Download Redis Repo"
 yum install epel-release yum-utils -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
 STATUS $?
 
-print "Set Up Redis Repo"
+print "Set Up Redis Repo\t\t"
 yum-config-manager --enable remi &>>$LOG
 STATUS $?
 
-print "Install Redis"
+print "Install Redis\t\t"
 yum install redis -y &>>$LOG
 STATUS $?
 
-print "Configuring Redis"
+print "Configuring Redis\t"
 sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf
 STATUS $?
 
-print "Start Redis Database"
+print "Start Redis Database\t"
 systemctl enable redis && systemctl restart redis &>>$LOG
 STATUS $?
