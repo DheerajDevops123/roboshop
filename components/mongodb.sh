267c5471966c4c090dc9de28e96fd9397a732e1c
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-05 09:07:19 +0000

    mongopush2

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -6,66 +6,32 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-fi
+STATUS
 
 echo "Installing MongoDB"
 yum install -y mongodb-org &>>/tmp/log
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-fi
+STATUS
 
 echo "Configuring MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-fi
+STATUS
 
 echo "Starting MongoDB"
 systemctl enable mongod
 systemctl restart mongod
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-fi
+STATUS
 
 echo "Downloading MongoDB Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-fi
+STATUS
 
 cd /tmp
 echo "Extracting Schema"
 unzip -o mongodb.zip &>>/tmp/log
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-fi
+STATUS
+
 cd mongodb-main
 echo "Loading Schema"
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js &>>/tmp/log
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-fi
+STATUS
