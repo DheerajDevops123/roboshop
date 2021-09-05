590eaf5fb3928073e575b3abd4a846934fc8d567
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-05 15:33:16 +0000

    mongopush2

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -31,7 +31,7 @@ STATUS $?
 chown roboshop:roboshop -R /home/roboshop
 
 print "Update SystemD Service"
-sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal" /home/roboshop/catalogue/systemd.service
+sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/catalogue/systemd.service
 
 print "Setup SystemD Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl start catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
