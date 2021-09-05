5748ac5faf08f9daf9a77128249fd3e57c48229d
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-05 09:33:29 +0000

    mongopush2

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -9,8 +9,11 @@ else
 fi
 }
 
-echo "Setting up MongoDB Repository"
+print() {
+    echo -n -e "$1 - "
+}
 
+print "Setting up MongoDB Repository"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -19,30 +22,30 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 STATUS $?
 
-echo "Installing MongoDB"
+print "Installing MongoDB"
 yum install -y mongodb-org &>>/tmp/log
 STATUS $?
 
-echo "Configuring MongoDB"
+print "Configuring MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 STATUS $?
 
-echo "Starting MongoDB"
+print "Starting MongoDB"
 systemctl enable mongod
 systemctl restart mongod
 STATUS $?
 
-echo "Downloading MongoDB Schema"
+print "Downloading MongoDB Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 STATUS $?
 
 cd /tmp
-echo "Extracting Schema"
+print "Extracting Schema"
 unzip -o mongodb.zip &>>/tmp/log
 STATUS $?
 
 cd mongodb-main
-echo "Loading Schema"
+print "Loading Schema"
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js &>>/tmp/log
 STATUS $?
