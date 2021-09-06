38047e678184f1083ce83ae6d41cd7984c81d8f3
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-06 17:27:12 +0000

    mongopush2

--- create-instance.sh
+++ create-instance.sh
@@ -9,9 +9,9 @@ echo "input is missing"
 exit 1
 fi
 
-InstanceID=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications 
+IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications 
 ResourceType=spot-instances-request,Tags=[{Key=Name,Value=INSTANCE_NAME} 
 ResourceType=instance,Tags=[{Key=Name,Value=INSTANCE_NAME} | 
-jq .Instances[].InstanceId | sed -e 's/"//g')
+jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
 
 
