f39186969869b76dd41606fef21753d27577a078
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-06 16:43:59 +0000

    mongopush2

--- create-instance.sh
+++ create-instance.sh
@@ -3,4 +3,4 @@
 LID="lt-0c39b2152102f71c7"
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER
+aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER | jq.Instances[].InstanceId
