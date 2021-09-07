aeeb8b49847a19d59af6d016896b655491f04889
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-07 09:56:57 +0000

    mongopush2

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 source components/common.sh
 
-print "Install NGINX"
+print "Install NGINX\t\t"
 yum install nginx -y &>>$LOG
 STATUS $?
 
-print "Download frontend HTML Doc"
+print "Download frontend HTML DOC"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 STATUS $?
 
 
-print "Extracting FrontEnd"
+print "Extracting FrontEnd\t"
 rm -rf /usr/share/nginx/* && cd /usr/share/nginx && unzip -o /tmp/frontend.zip  &>>$LOG  &&  mv frontend-main/* .  &>>$LOG  &&  mv static html &>>$LOG
 STATUS $?
 
-print "Update Roboshop Config"
+print "Update Roboshop Config\t"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 STATUS $?
 
-print "Restart NGINX"
+print "Restart NGINX\t\t"
 systemctl restart nginx &>>$LOG
 STATUS $?
