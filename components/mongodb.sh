61f338106384df180723462c1f14f9b2445d6e3e
Dheeraj Devops <sai.dheeraj.52056@gmail.com>
2021-09-08 02:08:15 +0000

    mongopush2

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -15,7 +15,7 @@ yum install -y mongodb-org &>>$LOG
 STATUS $?
 
 print "Configuring MongoDB\t"
-sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 STATUS $?
 
 print "Starting MongoDB\t"
